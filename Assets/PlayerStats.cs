17ea05e11b3b0cf6b1ecb5c62d7cce07026ece23
Nick Atkins <littledumbco@gmail.com>
2017-12-29 17:25:42 +0000

    Variables & UI Hookup
    Started toying around with the fundamental basics of variables, specifically looking at ints, strings and Text.
    
    Also wanted to start understanding how you can link those variables up with the frontend UI, so I built a basic script that works as some sort of player health checker.
    
    The UI displays a 'health status' that informs you how 'well' you are. I have also set it up so you can also see the HP and XP, which is handled by transforming the Int into a string.
    
    I realised pretty quickly that I needed to make that happen within the update function, so I created a function called StatUpdater that houses that functionality and runs within Update.
    
    To ensure that works, I added user input where you can incrementally increase or decrease the health by 50, which shows that it updates, but also updates the health status checker too.

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+	
+	public Text healthStatus;
+	private int myHP = 500;
+	private int myEXP = 5;
+	public Text uiHP;
+	public Text uiEXP;
+
+	// Use this for initialization
+	void Start () {
+
+		healthStatus.text = "Super Alive!";
+
+
+	}
+
+	// Script to transform the HP and EXP into text strings
+	void StatUpdater () {
+
+		uiHP.text = "Health: " + myHP.ToString ();
+		uiEXP.text = "XP: " + myEXP.ToString ();
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// Ensuring that the HP and XP are always updated into text
+		StatUpdater ();
+
+		// User based input to increase or decrease HP
+		if (Input.GetKeyDown (KeyCode.Space)) { myHP -= 50; }
+		if (Input.GetKeyDown (KeyCode.E)) { myHP += 50; }
+
+
+		// Updating the health status checker with relevant status
+		if ( myHP >= 500 ) { healthStatus.text = "You're swell"; } 
+		else if ( myHP == 250 ) { healthStatus.text = "You're hurting"; } 
+		else if ( myHP <= 0 ) { healthStatus.text = "You're dead"; }
+
+	}
+}
